84f5a8d091478b70ae0de9d89713eabfbb75eecd
Umberto <umbertobianchin.mail@gmail.com>
2024-05-17 11:44:34 +0200

    autenticazione quasi finita

--- app/src/main/java/it/unipd/dei/pseaiproject/MainActivity.kt
+++ app/src/main/java/it/unipd/dei/pseaiproject/MainActivity.kt
@@ -1,13 +1,35 @@
 package it.unipd.dei.pseaiproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.prova)
+        val button = findViewById<Button>(R.id.button)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        if(firebaseAuth.currentUser != null){
+            textView.text = firebaseAuth.currentUser!!.email.toString()
+        }
+
+        button.setOnClickListener{
+            firebaseAuth.signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
